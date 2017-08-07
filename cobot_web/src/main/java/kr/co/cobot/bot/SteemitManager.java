0edfffc99a7827a5ad2d6aee007731dec78eee46
nhj7 <nhj7@NHJ>
2017-08-08 01:29:14 +0900

    ggg

--- cobot_web/src/main/java/kr/co/cobot/bot/SteemitManager.java
+++ cobot_web/src/main/java/kr/co/cobot/bot/SteemitManager.java
@@ -463,7 +463,6 @@ public class SteemitManager implements Runnable {
     	String permLink = discussion.getPermlink();
     	double realAmt = 0.0;
     	if( method == 2 ){
-    		
     		realAmt = new BigDecimal(sellAmt - ( voteAmt * Double.parseDouble(voteRatio))).setScale(2, BigDecimal.ROUND_DOWN).doubleValue();
     	}else{
     		realAmt = sellAmt;
