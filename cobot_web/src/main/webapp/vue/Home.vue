6eb0ac14d5814b1a27df1fb10036186b69dbfe7b
nhj7 <nhj7@gmail.com>
2017-12-24 19:54:27 +0900

    update

---
+++ cobot_web/src/main/webapp/vue/Home.vue
@@ -0,0 +1,290 @@
+<template>
+<div>
+
+
+    <div class="row">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+              USD/KRW 1,080
+          </div>
+          <table class="table table-fixed">
+            <thead>
+              <tr>
+                <th class="col-xs-2 text-center">Ex</th>
+                <th class="col-xs-2 text-center">Coin</th>
+                <th class="col-xs-2 text-center">BTC</th>
+                <th class="col-xs-4 text-center">Flat</th>
+                <th class="col-xs-2 text-center">CH</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td class="col-xs-2 text-center"><img class="ex_icon" src="/img/exchange/2.png"></td>
+                <td class="col-xs-2 text-center">BTC</td>
+                <td class="col-xs-2 text-center">22.22%</td>
+                <td class="col-xs-4 text-right">19,445,500</td>
+                <td class="col-xs-2 text-center">+23</td>
+              </tr>
+              <tr>
+                <td class="col-xs-2 text-center"><img class="ex_icon" src="/img/exchange/1.png"></td>
+                <td class="col-xs-2 text-center">BTC</td>
+                <td class="col-xs-2 text-center">22.22%</td>
+                <td class="col-xs-4 text-right">19,445,500</td>
+                <td class="col-xs-2 text-center">+23</td>
+              </tr>
+
+              <tr>
+                <td class="col-xs-2 text-center"><img class="ex_icon" src="/img/exchange/3.png"></td>
+                <td class="col-xs-2 text-center">BTC</td>
+                <td class="col-xs-2 text-center">22.22%</td>
+                <td class="col-xs-4 text-right">19,445,500</td>
+                <td class="col-xs-2 text-center">+23</td>
+              </tr>
+              <tr>
+                <td class="col-xs-2 text-center"><img class="ex_icon" src="/img/exchange/4.png"></td>
+                <td class="col-xs-2 text-center">BTC</td>
+                <td class="col-xs-2 text-center">22.22%</td>
+                <td class="col-xs-4 text-right">19,445,500</td>
+                <td class="col-xs-2 text-center">+23</td>
+              </tr>
+              <tr>
+                <td class="col-xs-2 text-center"><img class="ex_icon" src="/img/exchange/5.png"></td>
+                <td class="col-xs-2 text-center">BTC</td>
+                <td class="col-xs-2 text-center">22.22%</td>
+                <td class="col-xs-4 text-right">19,445,500</td>
+                <td class="col-xs-2 text-center">+23</td>
+              </tr>
+              <tr>
+                <td class="col-xs-2 text-center"><img class="ex_icon" src="/img/exchange/6.png"></td>
+                <td class="col-xs-2 text-center">BTC</td>
+                <td class="col-xs-2 text-center">22.22%</td>
+                <td class="col-xs-4 text-right">19,445,500</td>
+                <td class="col-xs-2 text-center">+23</td>
+              </tr>
+              <tr>
+                <td class="col-xs-2 text-center"><img class="ex_icon" src="/img/exchange/1.png"></td>
+                <td class="col-xs-2 text-center">BTC</td>
+                <td class="col-xs-2 text-center">22.22%</td>
+                <td class="col-xs-4 text-right">19,445,500</td>
+                <td class="col-xs-2 text-center">+23</td>
+              </tr>
+              <tr>
+                <td class="col-xs-2 text-center"><img class="ex_icon" src="/img/exchange/1.png"></td>
+                <td class="col-xs-2 text-center">BTC</td>
+                <td class="col-xs-2 text-center">22.22%</td>
+                <td class="col-xs-4 text-right">19,445,500</td>
+                <td class="col-xs-2 text-center">+23</td>
+              </tr>
+              <tr>
+                <td class="col-xs-2 text-center"><img class="ex_icon" src="/img/exchange/1.png"></td>
+                <td class="col-xs-2 text-center">BTC</td>
+                <td class="col-xs-2 text-center">22.22%</td>
+                <td class="col-xs-4 text-right">19,445,500</td>
+                <td class="col-xs-2 text-center">+23</td>
+              </tr>
+              <tr>
+                <td class="col-xs-2 text-center"><img class="ex_icon" src="/img/exchange/1.png"></td>
+                <td class="col-xs-2 text-center">BTC</td>
+                <td class="col-xs-2 text-center">22.22%</td>
+                <td class="col-xs-4 text-right">19,445,500</td>
+                <td class="col-xs-2 text-center">+23</td>
+              </tr>
+              <tr>
+                <td class="col-xs-2 text-center"><img class="ex_icon" src="/img/exchange/1.png"></td>
+                <td class="col-xs-2 text-center">BTC</td>
+                <td class="col-xs-2 text-center">22.22%</td>
+                <td class="col-xs-4 text-right">19,445,500</td>
+                <td class="col-xs-2 text-center">+23</td>
+              </tr>
+              <tr>
+                <td class="col-xs-2 text-center"><img class="ex_icon" src="/img/exchange/1.png"></td>
+                <td class="col-xs-2 text-center">BTC</td>
+                <td class="col-xs-2 text-center">22.22%</td>
+                <td class="col-xs-4 text-right">19,445,500</td>
+                <td class="col-xs-2 text-center">+23</td>
+              </tr>
+              <tr>
+                <td class="col-xs-2 text-center"><img class="ex_icon" src="/img/exchange/1.png"></td>
+                <td class="col-xs-2 text-center">BTC</td>
+                <td class="col-xs-2 text-center">22.22%</td>
+                <td class="col-xs-4 text-right">19,445,500</td>
+                <td class="col-xs-2 text-center">+23</td>
+              </tr>
+
+            </tbody>
+          </table>
+        </div>
+    </div>
+
+</div>
+</template>
+
+<style>
+.table-fixed thead {
+  width: 98%;
+}
+.table-fixed tbody {
+  height: 75%;
+  overflow-y: auto;
+  width: 100%;
+}
+.table-fixed thead, .table-fixed tbody, .table-fixed tr, .table-fixed td, .table-fixed th {
+  display: block;
+}
+.table-fixed tbody td, .table-fixed thead > tr> th {
+  float: left;
+  border-bottom-width: 0;
+}
+
+.ex_icon{
+  width:18px;
+  height:16px;
+}
+</style>
+
+<script>
+
+var linkData = [
+  {
+    title : "Steeme"
+    , text : "Useful site to show hidden information."
+    , url : "https://ianpark.github.io/steeme/"
+    , creator : "asbear"
+  }
+  , {
+    title : "Steem Tool"
+    , text : "The Steemit tool suite."
+    , url : "http://tool.steem.world"
+    , creator : "segyepark"
+  }
+  , {
+    title : "Steem Us"
+    , text : "steemit stats and portals."
+    , url : "http://www.steemus.com"
+    , creator : "jungs"
+  }
+  , {
+    title : "MukSteem"
+    , text : "Korean Stimian's Restaurant Collection."
+    , url : "http://muksteem.com"
+    , creator : "lee2"
+  }
+
+  , {
+    title : "Under Steemed"
+    , text : "Undervalued posting excavation site."
+    , url : "https://sydneyitguy.github.io/understeemed"
+    , creator : "tabris"
+  }
+  , {
+    title : "Steem Friend"
+    , text : "Who would be best friend of me?"
+    , url : "http://steemfriend.com/"
+    , creator : "jeongmincha"
+  }
+  , {
+    title : "Steemit Post Statistics"
+    , text : "Steemit Post Statistics site."
+    , url : "https://iworldpark.github.io/steemit/stat/post/"
+    , creator : "segyepark"
+  }
+  , {
+    title : "Steem Now"
+    , text : "What if I wonder about my voting power? "
+    , url : "https://steemnow.com/"
+    , creator : "penguinpablo"
+  }
+
+  , {
+    title : "Steem Report"
+    , text : "Helping you make sense of Steemit, and your place within it."
+    , url : "http://www.steemreports.com/delegation-info/"
+    , creator : "steemreports"
+  }
+
+  , {
+    title : "Cobot"
+    , text : "All Coin Ticker. "
+    , url : "http://cobot.co.kr"
+    , creator : "nhj12311"
+  }
+  , {
+    title : "SteemChatBot"
+    , text : "New Steem Telegram Notification Tool. "
+    , url : "https://steemit.com/steemchatbot/@ludorum/steem-mentions-steemchatbot-v0-1-0"
+    , creator : "ludorum"
+  }
+
+
+];
+
+var linkDataDummy = linkData.slice(0);
+
+function init(){
+  //console.log(home.data.links);
+  for(var i = 0; i < linkData.length;i++){
+    linkData[i] = linkDataDummy[i];
+    //console.log("localStorage.getItem : "+localStorage.getItem(linkData[i].url));
+    if( localStorage.getItem(linkData[i].url) == undefined ){
+      localStorage.setItem(linkData[i].url, 0 );
+    }
+    linkData[i].clickCount = localStorage.getItem(linkData[i].url);
+  }
+}
+
+function sortLinks(){
+    linkData.stableSort( (a, b) => parseInt(b.clickCount) - parseInt(a.clickCount) );
+}
+
+init();
+sortLinks();
+
+var homeData = { acc : '12311' };
+
+var home = module.exports = {
+    name : "Home"
+    , data : function () {
+      return {
+        homeData: homeData
+        , links : linkData
+      }
+    }
+    , methods : {
+			goClick : function( link ) {
+				goClick(link);
+			}
+      , goCreator : function (link) {
+        goCreator(link);
+
+      }
+      , resetCount : function(){
+        resetCount();
+      }
+    }
+    , mounted : function(){
+      test();
+  }
+}
+
+function test(){
+  homeData.acc = "123123";
+}
+
+function goClick(link){
+  localStorage.setItem(link.url, ++link.clickCount);
+  sortLinks();
+  window.open(link.url);
+}
+
+function goCreator(link){
+  window.open("http://steemit.com/@"+link.creator);
+}
+
+function resetCount(){
+  init();
+  for(var i = 0; i < linkData.length;i++){
+    localStorage.setItem(linkData[i].url, 0 );
+    linkData[i].clickCount = 0;
+  }
+}
+</script>
